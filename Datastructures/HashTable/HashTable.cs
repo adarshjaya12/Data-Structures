c0fd345913de57e798fd36c3fac04715a150f1cb
adarsh <adarshjaya12@gmail.com>
2019-08-19 21:33:12 -0500

    Hash Table

---
+++ Datastructures/HashTable/HashTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures.HashTable
+{
+    public class KeyValuePair<K,V>
+    {
+        public K Key;
+        public V Value;
+    }
+
+    public class HashTable<K, V>
+    {
+        LinkedList<KeyValuePair<K, V>>[] KeyValuePairs;
+        int Size = 0;
+        public HashTable(int size)
+        {
+            Size = size;
+            KeyValuePairs = new LinkedList<KeyValuePair<K, V>>[Size];
+        }
+
+
+        public void Add()
+        {
+
+        }
+
+
+        public int GetIndexByKey(K key)
+        {
+            return key.GetHashCode() % Size;
+        }
+        
+    }
+}
